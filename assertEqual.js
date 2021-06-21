39fc1a0871b035924103a6d5af5962f2de6d7036
Rebekah <rebekahmparkes@outlook.com>
2021-06-21 23:48:23 +0000

    testing equalities

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed!");
+    console.log(`Assertion Passed! ${actual} equals ${expected}!`);
   } else {
-    console.log("Assertion Failed!");
+    console.log(`Assertion Failed! ${actual} doesn\'t equal ${expected}!`);
   }
 };
 
