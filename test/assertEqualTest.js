729b11af5f7b110a56abad8fb7f71893da73fb52
Rebekah <rebekahmparkes@outlook.com>
2021-06-28 19:52:21 +0000

    tesssssst and refactored

---
+++ test/assertEqualTest.js
@@ -0,0 +1,12 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+// ...
+
+
+assertEqual('this','this');
+assertEqual('this','that');
+assertEqual(5,5);
+assertEqual(5,5.1);
